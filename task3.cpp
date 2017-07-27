6fc5bc5091bad2ae979a8ffa3ff1509b712aa547
kirillproger <kvp.kv@bk.ru>
2017-07-27 22:15:43 +0400

    Add files via upload

---
+++ task3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <dlib/algs.h>
+using namespace std;
+using namespace dlib;
+int main() {
+	int x;
+	int a = 1;
+	int b = 100;
+	int val;
+	cin >> val;
+cout<<put_in_range(a,b,val)<<endl;
+
+	system("pause");
+	return 0;
+}
